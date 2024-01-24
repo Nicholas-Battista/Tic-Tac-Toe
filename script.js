e05a99e33a7cda530cdbb297b44c620f756e8a2a
Nicholas-Battista <battistan23@gmail.com>
2024-01-23 19:11:33 -0700

    created js functions to read in each player
    userX and userO functions as well as a start btn handler

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ const Gameboard = (() => {
     [4, 5, 6],
     [7, 8, 9],
   ];
+
   let liveBoard = [];
+
   const updateValue = (value, row, col) => {
     return (liveBoard[row][col] = value);
   };
+
   return { layout, liveBoard, updateValue };
 })();
 
@@ -24,3 +27,23 @@ const newGame = (function () {
     });
   });
 })();
+
+function handleStartBtn() {
+  const UserX = (() => {
+    const name = document.getElementById("playerX").value;
+    const symbol = "X";
+    return { name, symbol };
+  })();
+
+  const UserO = (() => {
+    const name = document.getElementById("playerO").value;
+    const symbol = "O";
+    return { name, symbol };
+  })();
+
+  console.log(UserX);
+  console.log(UserO);
+}
+
+const startBtn = document.querySelector(".start");
+startBtn.addEventListener("click", handleStartBtn);
